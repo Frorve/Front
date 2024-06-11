9c1a18c65e269912245ca652f2d165ab8265d8ad
Frorve <fvfotura@gmail.com>
2024-06-11 11:34:14 +0200

    Update 11/06

--- src/adapters/components/ChatBubble/Chat.jsx
+++ src/adapters/components/ChatBubble/Chat.jsx
@@ -55,7 +55,7 @@ const Chat = () => {
       <div className="relative">
         <button
           className="z-20 text-white flex flex-col shrink-0 grow-0 justify-around 
-                      fixed bottom-0 right-0 right-5 rounded-lg
+                      fixed bottom-0 right-5 rounded-lg
                       mr-1 mb-5 lg:mr-5 lg:mb-5 xl:mr-10 xl:mb-10"
           onClick={() => setIsChatOpen(!isChatOpen)}
         >
